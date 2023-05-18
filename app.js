7c4dcce526512054c377ca5e27bd2eff60e2a1d5
Robbie del Rosario <robbie@ubx.ph>
2023-05-18 09:04:59 +0800

    feat(setup): initial project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Health check endpoint
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'OK' });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
