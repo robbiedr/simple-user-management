3dc65bd94bbd4ddc8e13abfcb433f2d302b62a8d
Robbie del Rosario <robbie@ubx.ph>
2023-05-18 17:44:59 +0800

    feat(user service): added get user list or details

--- utils/auth.js
+++ utils/auth.js
@@ -8,13 +8,20 @@ const {TOKEN_SECRET_KEY} = process.env;
  * @param {object} req Request object
  * @param {object} res Response object
  * @param {object} next Next
+ * @return {object} next object to skip validation
  */
 function authenticateToken(req, res, next) {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
 
+  // if (!token) {
+  //   throw createError(401, 'Bearer token missing');
+  // }
+  // Changed this due to the need for user list/details
+  // Recommendation: Use the commented snippet above and communicate the pros and cons
   if (!token) {
-    throw createError(401, 'Bearer token missing');
+    req.user = null; // Set req.user to null when token is missing
+    return next();
   }
 
   jwt.verify(token, TOKEN_SECRET_KEY, (err, user) => {
