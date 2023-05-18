44e48bde306b384ad185b06680eef3038fdb76bb
Robbie del Rosario <robbie@ubx.ph>
2023-05-18 14:15:11 +0800

    feat(logs): improved loggers

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ const express = require('express');
 const app = express();
 const {APP_PORT} = process.env;
 const createError = require('http-errors');
+const logger = require('./utils/logger');
 
 // Import the users route
 const usersRouter = require('./routes/Users');
 
 app.use(express.json());
+
+// Custom middleware function to log requests
+const requestLoggerMiddleware = (req, res, next) => {
+  // Log the request details with API parameters
+  const {method, url, body, query} = req;
+  const params = {...body, ...query};
+  logger.info(`${method} ${url} - Parameters: ${JSON.stringify(params)}`);
+
+  // Pass the request to the next middleware or route handler
+  next();
+};
+
+// Use the request logger middleware for all requests
+app.use(requestLoggerMiddleware);
 
 // Health check endpoint
 app.get('/health', (req, res) => {
@@ -32,5 +47,5 @@ app.use((err, req, res, next) => {
 
 // Start the server
 app.listen(APP_PORT, () => {
-  console.log(`Server is running on port ${APP_PORT}`);
+  logger.info(`Server is running on port ${APP_PORT}`);
 });
